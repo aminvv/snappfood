ef5530a14b37391e54fa6f4548224735be40d4fa
aminvv <aminviper1378@gmail.com>
2024-12-26 12:07:58 +0330

    config S3 service ------------>>>>>>>>>>>>>(chapter:9 section:6-7)

--- src/common/typeDefination/env.d.ts
+++ src/common/typeDefination/env.d.ts
@@ -1,14 +1,19 @@
-namespace NodeJS{
+namespace NodeJS {
     interface ProcessEnv {
         // LOCAL PORT
-        PORT:number,
-        
+        PORT: number
+
         //  DATABASE
-        DB_PORT:number,
-        DB_HOST:string,
-        DB_NAME:string,
-        DB_USERNAME:string,
-        DB_PASSWORD:string,
-       
+        DB_PORT: number
+        DB_HOST: string
+        DB_NAME: string
+        DB_USERNAME: string
+        DB_PASSWORD: string
+
+        // S3 SECRET
+        S3_SECRET_KEY: string
+        S3_ACCESS_KEY: string
+        S3_BUCKET_NAME: string
+        S3_ENDPOINT: string
     }
 }
